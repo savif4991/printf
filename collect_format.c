a55fad0d97783b7fb9fa466450a97345c967fb68
Daejin Lee <daejlee@c4r2s4.42seoul.kr>
2022-06-22 20:47:03 +0900

    commit

--- collect_format.c
+++ collect_format.c
@@ -11,10 +11,31 @@
 /* ************************************************************************** */
 #include "ft_printf.h"
 
-int	collect_format(const char *str, struct s_info *p, va_list ap)
+static unsigned int	get_width(struct s_info *p)
 {
 	unsigned int	i;
 	char			*temp;
+
+	temp = ft_itoa(p->width);
+	i = ft_strlen(temp);
+	free(temp);
+	return (i);
+}
+
+static unsigned int	get_precision(struct s_info *p)
+{
+	unsigned int	i;
+	char			*temp;
+
+	temp = ft_itoa(p->precision);
+	i = ft_strlen(temp) + 1;
+	free(temp);
+	return (i);
+}
+
+int	collect_format(const char *str, struct s_info *p, va_list ap)
+{
+	unsigned int	i;
 
 	i = 0;
 	p->flag = check_flag(&str[i]);
@@ -22,18 +43,10 @@ int	collect_format(const char *str, struct s_info *p, va_list ap)
 		i += ft_strlen(p->flag);
 	p->width = check_width(&str[i], ap);
 	if (p->width != -1)
-	{
-		temp = ft_itoa(p->width);
-		i += ft_strlen(temp);
-		free(temp);
-	}
+		i += get_width(p);
 	p->precision = check_precision(&str[i], ap);
 	if (p->precision != -1 && p->precision != -2)
-	{
-		temp = ft_itoa(p->precision);
-		i += ft_strlen(temp) + 1;
-		free(temp);
-	}
+		i += get_precision(p);
 	else if (p->precision == -2)
 		i++;
 	p->raw_str = check_specifier(ap, &str[i]);
